df34f64fa99d057fa3c71366be59612e1ab4a4ed
Alfonso <alfonsoderojasperez@gmail.com>
2022-03-30 09:50:27 +0200

    maquetacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { FiltrosComponent } from './filtros/filtros.component';
+import { TablaComponent } from './tabla/tabla.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    FiltrosComponent,
+    TablaComponent
   ],
   imports: [
     BrowserModule,
